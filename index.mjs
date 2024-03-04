261e72e8e52d1dbc3f81d2c6a2a9c61b0effb924
Alexander Gabriel <alex.barbalex@gmail.com>
2024-03-04 17:52:34 +0100

    clean up

--- index.mjs
+++ index.mjs
@@ -7,27 +7,18 @@ import moment from 'moment'
 import DatauriParser from 'datauri/parser.js'
 const parser = new DatauriParser()
 
+// 1. set the top directory to begin looking in
+// const dataDir =
+//   'D:\\Dropbox\\Familienbereich\\Gabriel_Multimedia\\Bilder\\2017\\costa_rica_renaming'
+const dataDir =
+  'D:\\Dropbox\\Familienbereich\\Gabriel_Multimedia\\Bilder\\2017\\2017 07 08 von olympus Costa Rica'
 // logging to file and console: https://stackoverflow.com/a/21061306/712005
 var logFile = fs.createWriteStream(
-  'D:\\Dropbox\\Familienbereich\\Gabriel_Multimedia\\Bilder\\2017\\costa_rica_renaming\\renaming.log',
+  `${dataDir}\\renaming_${moment(Date.now(), 'YYYY:MM:DD_HH:mm:ss')}.log`,
   { flags: 'w' },
 )
 const logStdout = process.stdout
 
-// 1. set the top directory to begin looking in
-// const dataDir = 'C:\\Users\\alex\\jpg-renamer\\testdata'
-const dataDirs = [
-  '2017 08 2 Cahuita',
-  '2017 08 1 Tortuguero',
-  '2017 08 4 Laguna del Lagarto',
-  '2017 08 5 La Fortuna',
-  '2017 08 6 Monteverde',
-  '2017 08 7 Rincon de la Vieja',
-  '2017 08 8 Samara',
-  '2017 08 9 Quepos',
-]
-const dataDir =
-  'D:\\Dropbox\\Familienbereich\\Gabriel_Multimedia\\Bilder\\2017\\costa_rica_renaming'
 // use this line instead of above to test
 //const dataDir = `${process.cwd()}\\testdata`
 
@@ -46,6 +37,7 @@ const timeDiff = 16 * 60 * 60 * 1000 + 1 * 60 * 1000 + 54 * 1000
 
 let count = 0
 let skippedDueToMimeCount = 0
+let skippedDueToMissingExifDateCount = 0
 let skippedDueToCorrectNameCount = 0
 let renamedCount = 0
 let timeShiftedCount = 0
@@ -53,12 +45,14 @@ let timeShiftedCount = 0
 async function executeForAllFilesInDir(dir) {
   // reads all files in directory...
   const files = fs.readdirSync(dir)
+  console.log(`${dir} contains ${files.length} files.`)
   // ...and loop them
   for await (const fileName of files) {
     const filePath = `${dir}\\${fileName}`
     // call this function recursively for contained directories
     if (fs.statSync(filePath).isDirectory()) {
-      executeForAllFilesInDir(filePath)
+      console.log(`Entering directory ${filePath}`)
+      await executeForAllFilesInDir(filePath)
       continue
     }
     count++
@@ -69,14 +63,20 @@ async function executeForAllFilesInDir(dir) {
       console.log(logMessage)
       logFile.write(format(logMessage) + '\n')
       skippedDueToMimeCount++
-      return
+      continue
     }
     const file = fs.readFileSync(filePath)
     // piexif wants to receive a base-64 formatted string
     const parsedFile = parser.format('.jpg', file)
     const exifData = piexif.load(parsedFile.content)
     const dateTimeString = exifData.Exif[36867]
-    if (!dateTimeString) continue
+    if (!dateTimeString) {
+      const logMessage = `Skipping ${filePath}, as no exif date found`
+      console.log(logMessage)
+      logFile.write(format(logMessage) + '\n')
+      skippedDueToMissingExifDateCount++
+      continue
+    }
 
     let dateTime = moment(dateTimeString, 'YYYY:MM:DD HH:mm:ss')
     // compensate for wrong time on camera
@@ -98,19 +98,19 @@ async function executeForAllFilesInDir(dir) {
       console.log(logMessage)
       logFile.write(format(logMessage) + '\n')
       fs.renameSync(filePath, newPath)
-      renamedCount++
+      changeTime ? timeShiftedCount++ : renamedCount++
       continue
     }
     const logMessage = `Skipping '${filePath}' as name is correct`
     console.log(logMessage)
     logFile.write(format(logMessage) + '\n')
-    renamedCount
+    skippedDueToCorrectNameCount++
     continue
   }
   return
 }
 await executeForAllFilesInDir(dataDir)
 
-// const logMessage = `Checked ${count} files, skipped ${skippedDueToMimeCount} files due to mime type and ${skippedDueToCorrectNameCount} files due to correct name, renamed ${renamedCount} files and time-shifted ${timeShiftedCount} files.`
-// console.log(logMessage)
-// logFile.write(format(logMessage) + '\n')
+const logMessage = `Checked ${count} files, skipped ${skippedDueToMimeCount} files due to mime type and ${skippedDueToCorrectNameCount} files due to correct name, renamed ${renamedCount} files and time-shifted ${timeShiftedCount} files.`
+console.log(logMessage)
+logFile.write(format(logMessage) + '\n')
